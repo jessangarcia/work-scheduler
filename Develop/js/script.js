ea4f307ae1402a46b35c25d674d673b5f780a3f2
jessan <jessenia.garcia998@gmail.com>
2021-07-17 20:28:18 -0600

    adding color to past present and future events

--- Develop/js/script.js
+++ Develop/js/script.js
@@ -1,12 +1,4 @@
-var hour9 = $("#9");
-var hour10 = $("#10");
-var hour11 = $("#11");
-var hour12 = $("#12");
-var hour1 = $("#1");
-var hour2 = $("#2");
-var hour3 = $("#3");
-var hour4 = $("#4");
-var hour5 = $("#5");
+
 var time = moment();
 
 //adding the current day to the top of the planner
@@ -19,23 +11,44 @@ function openPlanner() {
         var schedule = localStorage.getItem(load);
 
         if (schedule !== null) {
+            //https://api.jquery.com/children/#children-selector
             $(this).children(".schedule").val(schedule);
         }
     });
 }
 
-//calling time to top of page
+//calling time and saved events
 openPlanner();
 
 //saving text to localstorage 
 $(".saveBtn").on("click", function () {
     //https://api.jquery.com/parent/#parent-selector
     var time = $(this).parent().attr("id");
+    //https://api.jquery.com/siblings/#siblings-selector
     var schedule = $(this).siblings(".schedule").val();
 
     localStorage.setItem(time, schedule);
 })
 
+//changing the section colors
+function dueTimes() {
+    hour = time.hours();
+    $(".time-block").each(function() {
+        var currentHour = parseInt($(this).attr("id"));
+
+        if (currentHour > hour) {
+            $(this).addClass("future")
+        } 
+        else if (currentHour === hour) {
+            $(this).addClass("present")
+        } 
+        else {
+            $(this).addClass("past")
+        }
+    });
+}
+
+dueTimes();
 
 
 
